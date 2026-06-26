cd15b6de2b22b4710caf45a8f63ba0414bee27e8
StyearX <nazzz9442@gmail.com>
2026-06-26 14:34:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
-const REPO_NAME = 'Fluent-Modded-Docs';
-
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${REPO_NAME}/` : '/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
